30f51c2b09f873fcef7645d30a0711bf0a7982d3
rrich-kray <rrich.kray@gmail.com>
2022-01-14 07:33:40 -0500

    added inquirer prompt

--- app.js
+++ app.js
@@ -1,13 +1,26 @@
-const fs = require('fs')
-const generatePage = require('./src/page-template.js')
+// Be consistent and don't mix ES5 with ES6 syntax
 
-const profileDataArgs = process.argv.slice(2, process.argv.length);
+const inquirer = require('inquirer');
+console.log(inquirer)
 
-// const [name, github] = profileDataArgs;
+// const fs = require('fs')
+// const generatePage = require('./src/page-template.js')
 
-fs.writeFile('index.html', generatePage('john', 'johnsgit'),  err => {
-    if (err) throw err;
-})
+// const pageHTML = generatePage(name, github)
 
+// // const [name, github] = profileDataArgs;
 
+// fs.writeFile('./index.html', pageHTML,  err => {
+//     if (err) throw err;
+//     console.log('Saved!');
+// })
 
+inquirer
+    .prompt([
+        {
+            type: 'input',
+            name: 'name',
+            message: 'What is your name?'
+        }
+    ])
+    .then(answers => console.log(answers))
