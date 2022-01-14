b0362bf8ae1166e4956451eead518b24083d73ee
rrich-kray <rrich.kray@gmail.com>
2022-01-14 10:36:05 -0500

    added input validation

--- app.js
+++ app.js
@@ -1,26 +1,81 @@
 // Be consistent and don't mix ES5 with ES6 syntax
 
-const inquirer = require('inquirer');
-console.log(inquirer)
+const inquirer = require('inquirer'); // import inquirer module
 
-// const fs = require('fs')
-// const generatePage = require('./src/page-template.js')
-
-// const pageHTML = generatePage(name, github)
-
-// // const [name, github] = profileDataArgs;
-
-// fs.writeFile('./index.html', pageHTML,  err => {
-//     if (err) throw err;
-//     console.log('Saved!');
-// })
-
-inquirer
-    .prompt([
+const promptUser = () => {
+    return inquirer.prompt([
         {
             type: 'input',
             name: 'name',
-            message: 'What is your name?'
+            message: 'What is your name?',
+            validate: nameInput => {
+                if (nameInput) {
+                    return true;
+                } else {
+                    console.log('Please enter your name!');
+                    return false;
+                }
+            }
+        },
+        {
+            type: 'input',
+            name: 'github',
+            message: 'Enter your github username'
+        },
+        {
+            type: 'input',
+            name: 'about',
+            message: 'Provide some information about yourself:'
         }
     ])
-    .then(answers => console.log(answers))
+}
+
+const promptProject = portfolioData => {
+    if (!portfolioData.projects){portfolioData.projects = []}
+    inquirer.prompt([
+        {
+            type: 'input',
+            name: 'name',
+            message: 'What is the name of your project?'
+        },
+        {
+            type: 'input',
+            name: 'description',
+            message: 'Please provide a description of the project (required)'
+        },
+        {
+            type: 'checkbox',
+            name: 'languages',
+            message: 'What did you build this project with? (Check all that apply)',
+            choices: ['JavaScript', 'HTML', 'CSS', 'ES6', 'JQuery', 'Bootstrap', 'Node']
+        },
+        {
+            type: 'input',
+            name: 'link',
+            message: 'Enter the GitHub link to your project (required)'
+        },
+        {
+            type: 'confirm',
+            name: 'feature',
+            message: 'Would you like to feature this project?',
+            default: false
+        },
+        {
+            type: 'confirm',
+            name: 'confirmAddProject',
+            message: 'Would you like to enter another project?',
+            default: false
+        },
+    ]).then(projectData => {
+        portfolioData.projects.push(projectData);
+        if (projectData.confirmAddProject) {
+            return promptProject(portfolioData);
+        } else {
+            return portfolioData;
+        }
+    })
+}
+
+promptUser()
+    .then(promptProject)
+    .then(portfolioData => {console.log(portfolioData)})
